7ce2a1aee0aa1860ca0a1ae751d44ef00f7c062b
mhcrnl <mhcrnl@localhost.localdomain>
2020-11-13 18:31:53 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Fedora 32 Gtk+-3.24.23 with C -Window
+
+Install
+
+	git clone
+	cd 
+	./run.sh
---
+++ run.sh
@@ -0,0 +1,1 @@
+cc `pkg-config --cflags gtk+-3.0` salut.c -o hello `pkg-config --libs gtk+-3.0` && ./hello
